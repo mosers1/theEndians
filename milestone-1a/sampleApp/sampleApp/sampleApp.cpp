87e0ef3fcedbeabf1112ed311386ef45e36b9bd4
Scott Moser <mosers1@seattleu.edu>
2018-04-08 20:39:48 -0700

    Initial commit of sample project for milestone 1a.

---
+++ milestone-1a/sampleApp/sampleApp/sampleApp.cpp
@@ -0,0 +1,28 @@
+//==============================================================================
+// AUTHORS: Andrew Croneberger, Maggie Dong, Scott Moser
+//
+// FILENAME: sampleApp.cpp
+// DATE: 04/08/2018
+// REVISION HISTORY: 1.0.0
+// REFERENCES: None.
+//
+// DESCRIPTION:
+// A simple application that prints "Hello, World!" to the console.
+//
+// REQUIREMENTS:
+//   1. None.
+//
+// ASSUMPTIONS:
+//   1. C++ is an acceptable language to use.
+//
+// KNOWN ISSUES:
+//   1. None.
+//==============================================================================
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
