d5ca77ba0045b8d218106d02989478eeb552603a
Scott Moser <mosers1@seattleu.edu>
2018-05-20 15:51:00 -0700

    Adding prototype student Kiosk controller and view.

---
+++ 5051/Controllers/KioskController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _5051.Controllers
+{
+    /// <summary>
+    /// The kiosk student check in/out screen that will run in the classroom
+    /// </summary>
+    public class KioskController : Controller
+    {
+
+        /// <summary>
+        /// Return the list of students with the status of logged in or out
+        /// </summary>
+        /// <returns></returns>
+        // GET: Kiosk
+        public ActionResult Index()
+        {
+            // TODO: Add logic...
+            return View();
+        }
+
+        // GET: Kiosk/SetLogout/5
+        public ActionResult SetLogin(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return RedirectToAction("Error", "Home", "Invalid Data");
+            }
+
+            // TODO: Do something...
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: Kiosk/SetLogout/5
+        public ActionResult SetLogout(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return RedirectToAction("Error", "Home", "Invalid Data");
+            }
+
+            // TODO: Do something...
+
+            return RedirectToAction("Index");
+        }
+    }
+}
