cc0885b13e80a3c1aef1385c6195c83d053ac151
Kevin <cushing.kev@gmail.com>
2018-05-20 17:02:16 -0700

    Added Admin Panel controller and view

---
+++ 5051/Controllers/AdminPanelController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _5051.Controllers
+{
+    public class AdminPanelController : Controller
+    {
+        // GET: /AdminPanel/Options
+        public ActionResult Options()
+        {
+            ViewBag.Message = "Admin Options Panel";
+            return View();
+        }
+    }
+}
