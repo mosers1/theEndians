7d2bb340fc17d65740f53b20e2e2bfc5f211c10e
Kevin <cushing.kev@gmail.com>
2018-05-20 18:19:41 -0700

    Created view for viewing student report remotely

---
+++ 5051/Controllers/RemoteStudentController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _5051.Controllers
+{
+    public class RemoteStudentController : Controller
+    {
+        // GET: /AdminPanel/Options
+        public ActionResult Report()
+        {
+            ViewBag.Message = "View student report online (not via kiosk)";
+            return View();
+        }
+    }
+}
