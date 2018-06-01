c67fc791b1b05684e321113188ab8e2109866d32
Andrew <acroneberger@gmail.com>
2018-05-31 23:17:37 -0700

    added controller logic to draw multiple avatars

--- 5051/Controllers/RemoteStudentController.cs
+++ 5051/Controllers/RemoteStudentController.cs
@@ -30,13 +30,21 @@ namespace _5051.Controllers
         {
             return View();
         }
+        public string[] avatar_names = new string[]
+        {
+            "person1.png", "person2.png", "person3.png",
+            "person4.png", "person5.png", "person6.png"
+        };
+        
         //Returns Avatar select page
         public ActionResult ChooseAvatar()
         {
+            
             // Query backend to refresh every time Index() is called
             var myDataList = StudentBackend.Index();
             var StudentViewModel = new StudentViewModel(myDataList);
             var StudentModel = new StudentModel(StudentViewModel.StudentList[0]);
+            ViewBag.avatar_names = avatar_names;
             
             return View(StudentModel);
         }
